89f32e1767b9afc68296c53d263979032f87c153
narisettiashok <nleoashok5555@gmail.com>
2022-05-24 22:25:33 +0530

    header,home,about page - commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
+declare var $: any;
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ServicesComponent } from './services/services.component';
+import { WorksComponent } from './works/works.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    ServicesComponent,
+    WorksComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// declare var $ :any;
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  themeButton:any;
+  lightTheme: any;
+  iconTheme:any;
+  selectedTheme:any;
+  selectedIcon:any;
+  activeLink:any;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    /*=============== LIGHT DARK THEME ===============*/ 
+    this.themeButton = document.getElementById('theme-button')
+    this.lightTheme = 'light-theme'
+    this.iconTheme = 'bx-sun'
+
+    // Previously selected topic (if user selected)
+    this.selectedTheme = localStorage.getItem('selected-theme')
+    this.selectedIcon = localStorage.getItem('selected-icon')
+
+    // We obtain the current theme that the interface has by validating the light-theme class
+    this.getCurrentTheme();
+    this.getCurrentIcon();
+    // const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+    // const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+    // We validate if the user previously chose a topic
+    if (this.selectedTheme) {
+      // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+      document.body.classList[this.selectedTheme === 'dark' ? 'add' : 'remove'](this.lightTheme)
+      this.themeButton.classList[this.selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](this.iconTheme)
+    }
+
+    $('.active-link-home').addClass('active-link')
+
   }
+
+  getCurrentTheme() {
+    return document.body.classList.contains(this.lightTheme) ? 'dark' : 'light';
+  }
+
+  getCurrentIcon() {
+    return this.themeButton.classList.contains(this.iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+  }
+
+  changeTheme() {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(this.lightTheme)
+    this.themeButton.classList.toggle(this.iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', this.getCurrentTheme())
+    localStorage.setItem('selected-icon', this.getCurrentIcon())
+  }
+
+  setActiveLink(item:string) {
+    this.activeLink = document.querySelectorAll('.nav_menu_bar_link');
+    this.activeLink.forEach((link: { classList: { remove: (arg0: string) => any; }; }) => link.classList.remove('active-link'));
+    // $('.active-link-about').removeClass('active-link');
+    $(`.active-link-${item}`).addClass('active-link');
+  }  
 
 }
